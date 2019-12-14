af8741b7926e3061bda9e249b0afcb4d3c4ab95c
Phil DeLong <philjdelong@gmail.com>
2019-12-14 12:00:22 -0700

    Update models to include has many belongs to

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ApplicationRecord
+  has_many :books
+
+  validates_presence_of :name
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+  belongs_to :author
+
+  validates_presence_of :title, :pages, :publication_year
 end
