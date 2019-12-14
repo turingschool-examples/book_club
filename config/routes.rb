6000a3cd81988b295903c156f4d5dda934f8fae6
Phil DeLong <philjdelong@gmail.com>
2019-12-14 12:17:40 -0700

    Add books index to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :books, only: :index
-  resources :authors, only: :show do
-    resources :books, only: [:new, :create]
-  end
+  # resources :books, only: :index
+  # resources :authors, only: :show do
+  # resources :books, only: [:new, :create]
+
+  get '/books', to: "books#index"
 end
