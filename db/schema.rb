37e8dc15041ccd54ec7e194e58112beb8f6419af
Phil DeLong <philjdelong@gmail.com>
2019-12-14 12:17:27 -0700

    Add author reference to books and update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190709193155) do
+ActiveRecord::Schema.define(version: 20191214191537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "publication_year"
     t.integer "pages"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "author_id"
+    t.index ["author_id"], name: "index_books_on_author_id"
   end
 
+  add_foreign_key "books", "authors"
 end
