c213bccebffb9a8c33bc106bf9d2b5d7bcf76d9a
sasloan <sebastiansloan94@gmail.com>
2019-12-14 19:40:59 -0700

    Update models to include validations and relationships

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ApplicationRecord
+	validates_presence_of :name
+
+	has_many :book_authors
+	has_many :authors, through: :book_authors
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,6 @@
 class Book < ApplicationRecord
+	validates_presence_of :title, :publication_year, :pages
+
+	has_many :book_authors
+	has_many :authors, through: :book_authors
 end
