fdb368a55c3e959e12678d32c47b94664e5e432f
sasloan <sebastiansloan94@gmail.com>
2019-12-14 21:12:42 -0700

    update from remote master

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/', to: 'books#index'
 	get '/books', to: 'books#index'
-	
+
   resources :books, only: :index
   resources :authors, only: :show do
     resources :books, only: [:new, :create]
