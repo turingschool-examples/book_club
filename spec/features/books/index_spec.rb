9a9b9e50e64d47a08b4246de47c1fdf7124cb6d1
sasloan <sebastiansloan94@gmail.com>
2019-12-14 21:19:46 -0700

    Create test to see if author name is a link

--- spec/features/books/index_spec.rb
+++ spec/features/books/index_spec.rb
@@ -46,5 +46,56 @@ describe 'As a User' do
 			expect(page).to_not have_content("2000")
 			expect(page).to_not have_content(517)
 		end
+
+		it 'Can see the author for each book as a link to the authors show page' do
+			@hpst = Book.create!(title: "Harry Potter and the Socerer's Stone",
+													 publication_year: "1997",
+												 	 pages: 223)
+
+			@hpcs = Book.create!(title: "Harry Potter and the Chamber of Secrets",
+													 publication_year: "1998",
+												 	 pages: 251)
+
+			@hppa = Book.create!(title: "Harrry Potter and the Prisoner of Azkaban",
+													 publication_year: "1999",
+												 	 pages: 317)
+
+			@rowling = @hpst.author.create!(name: "J.K. Rowling")
+
+			@rowling = @hpcs.author.create!(name: "J.K. Rowling")
+
+			@rowling = @hppa.author.create!(name: "J.K. Rowling")
+
+			@wb = @hpst.author.create!(name: "Warner Brothers Studios")
+
+			visit '/books'
+
+			expect(page).to have_content(@hpst.title)
+			expect(page).to have_content(@hpst.publication_year)
+			expect(page).to have_content(@hpst.pages)
+
+			expect(page).to have_content(@hpcs.title)
+			expect(page).to have_content(@hpcs.publication_year)
+			expect(page).to have_content(@hpcs.pages)
+
+			expect(page).to have_content(@hppa.title)
+			expect(page).to have_content(@hppa.publication_year)
+			expect(page).to have_content(@hppa.pages)
+
+			expect(page).to have_content(@rowling.name)
+			expect(page).to have_content(@wb.name)
+
+			expect(page).to_not have_content("Harry Potter and the Goblet of Fire")
+			expect(page).to_not have_content("2000")
+			expect(page).to_not have_content(517)
+
+			click_link "#{@rowling.name}"
+
+			expect(path).to eq("/authors/#{@rowling.id}")
+
+			click_link "#{@wb.name}"
+
+			expect(path).to eq("/authors/#{@wb.id}")
+		end
 	end
 end
