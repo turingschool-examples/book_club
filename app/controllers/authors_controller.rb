c2b4b7226593a42d69278558697bb3c5f571f0c6
sasloan <sebastiansloan94@gmail.com>
2019-12-14 21:57:10 -0700

    Create authors show page view

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -3,4 +3,8 @@ class AuthorsController < ApplicationController
 	def index
 		@authors = Author.all
 	end
+
+	def show
+		@author = Author.find(params[:id])
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get '/', to: 'books#index'
 	get '/books', to: 'books#index'
 	get '/authors', to: 'authors#index'
+	get '/authors/:id', to: 'authors#show'
 
   resources :books, only: :index
   resources :authors, only: :show do
