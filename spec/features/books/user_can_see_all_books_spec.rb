f644d612055fadf6ddec7572fe0e016f735c1c5d
Lain McGratH <lain.mcgrath@gusto.com>
2019-12-14 18:22:47 -0700

    Add book index spec

--- spec/features/books/user_can_see_all_books_spec.rb
+++ spec/features/books/user_can_see_all_books_spec.rb
@@ -2,27 +2,40 @@ require 'rails_helper'
 
 RSpec.describe "User can see all books" do
   it "can see all books" do
-    book_1 = Book.create( title: "All the Light We Cannot See",
-                          pages: 544,
-                          publication_year: 2014)
 
-    book_2 = Book.create( title: "The Nightingale",
-                          pages: 608,
-                          publication_year: 2017)
+    author_1 = Author.create!(name: "Anthony Doerr")
+    author_2 = Author.create!(name: "Stieg Larson")
 
+    book_1 = author_1.books.create!(title: "All the Light We Cannot See",
+                                    pages: 544,
+                                    publication_year: 2014)
+
+    book_2 = author_1.books.create (title: "About Grace",
+                                    pages: 432,
+                                    publication_year: 2015)
+
+    book_3 = author_2.books.create!(title: "The Girl with the Dragon Tattoo",
+                                    pages: 463,
+                                    publication_year: 2005)
+
+    book_4 = author_2.books.create!(title: "The Girl Who Played With Fire",
+                                    pages: 631,
+                                    publication_year: 2006)
+
+    book_5 = author_2.books.create!(title: "The Girl Who Kick the Hornet's Nest",
+                                    pages: 602,
+                                    publication_year: 2007)
     visit "/books"
 
     within "#book-#{book_1.id}" do
       expect(page).to have_content("All the Light We Cannot See")
       expect(page).to have_content(544)
       expect(page).to have_content(2014)
-      expect(page).to have_content("Anthony Doerr")
 
     within "#book-#{book_2.id}" do
       expect(page).to have_content("The Nightingale")
       expect(page).to have_content(608)
       expect(page).to have_content(2017)
-      expect(page).to have_content("Kristin Hannah")
     end
     end
   end
