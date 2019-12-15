e6ac421730421e9e714647d013734ce6d9790f2c
sasloan <sebastiansloan94@gmail.com>
2019-12-14 21:10:41 -0700

    Add author finding capability

--- spec/features/books/index_spec.rb
+++ spec/features/books/index_spec.rb
@@ -4,6 +4,7 @@ describe 'As a User' do
 	describe 'I can visit the books index page' do
 
 		it 'Can see all the books and their title, pub. year, and pages' do
+
 			@hpst = Book.create!(title: "Harry Potter and the Socerer's Stone",
 													 publication_year: "1997",
 												 	 pages: 223)
@@ -15,6 +16,14 @@ describe 'As a User' do
 			@hppa = Book.create!(title: "Harrry Potter and the Prisoner of Azkaban",
 													 publication_year: "1999",
 												 	 pages: 317)
+
+			@rowling = @hpst.author.create!(name: "J.K. Rowling")
+
+			@rowling = @hpcs.author.create!(name: "J.K. Rowling")
+
+			@rowling = @hppa.author.create!(name: "J.K. Rowling")
+
+			@wb = @hpst.author.create!(name: "Warner Brothers Studios")
 
 			visit '/books'
 
@@ -29,6 +38,9 @@ describe 'As a User' do
 			expect(page).to have_content(@hppa.title)
 			expect(page).to have_content(@hppa.publication_year)
 			expect(page).to have_content(@hppa.pages)
+
+			expect(page).to have_content(@rowling.name)
+			expect(page).to have_content(@wb.name)
 
 			expect(page).to_not have_content("Harry Potter and the Goblet of Fire")
 			expect(page).to_not have_content("2000")
