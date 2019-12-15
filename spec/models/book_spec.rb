dc73d93f57c0fd3128b47f1d40e20e2b8bd39dd4
sasloan <sebastiansloan94@gmail.com>
2019-12-14 19:25:42 -0700

    Created model test for books

---
+++ spec/models/book_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe Book, type: :model do
+	describe 'validations' do
+		it {should validate_presence_of :title}
+		it {should validate_presence_of :publication_year}
+		it {should validate_presence_of :pages}
+	end
+
+	describe 'relationships' do
+		it {should have_many :authors}
+		it {should have_many :book_authors}
+end
