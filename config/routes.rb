6af67afa4af2142cfbc944249b72959afb5bd3b1
Lain McGratH <lain.mcgrath@gusto.com>
2019-12-14 19:38:25 -0700

    Add /books route

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :books, only: :index
   resources :authors, only: :show do
     resources :books, only: [:new, :create]
+
+    get "/books", to: "books#index"
   end
 end
