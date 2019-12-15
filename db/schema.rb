564ff98462510418d59267a4717c8c5221bfbb50
Lain McGratH <lain.mcgrath@gusto.com>
2019-12-14 19:05:36 -0700

    Add updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191214175744) do
+ActiveRecord::Schema.define(version: 20191215015328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "author_books", force: :cascade do |t|
+    t.bigint "author_id"
+    t.bigint "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_author_books_on_author_id"
+    t.index ["book_id"], name: "index_author_books_on_book_id"
+  end
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -25,21 +34,6 @@ ActiveRecord::Schema.define(version: 20191214175744) do
     t.integer "pages"
   end
 
-  create_table "collection_books", force: :cascade do |t|
-    t.bigint "book_id"
-    t.bigint "collection_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["book_id"], name: "index_collection_books_on_book_id"
-    t.index ["collection_id"], name: "index_collection_books_on_collection_id"
-  end
-
-  create_table "collections", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_foreign_key "collection_books", "books"
-  add_foreign_key "collection_books", "collections"
+  add_foreign_key "author_books", "authors"
+  add_foreign_key "author_books", "books"
 end
