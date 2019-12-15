21a4bf5e148b9aa2d031c74140b4525e35bda3bb
Lain McGratH <lain.mcgrath@gusto.com>
2019-12-14 19:08:49 -0700

    Correct test from belongs_to to belong_to

--- spec/models/author_book_spec.rb
+++ spec/models/author_book_spec.rb
@@ -2,8 +2,7 @@ require 'rails_helper'
 
 RSpec.describe AuthorBook, type: :model do
   describe "relationships" do
-    it {should have_many}
-    it {should belong_to :collection}
+    it {should belong_to :author}
     it {should belong_to :book}
   end
 end
