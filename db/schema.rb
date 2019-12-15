a90eb6c60a1953001cb26ddae3bf769b079a6d8a
Lain McGratH <lain.mcgrath@gusto.com>
2019-12-14 17:57:44 -0700

    Add updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190709193155) do
+ActiveRecord::Schema.define(version: 20191214175744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,4 +25,21 @@ ActiveRecord::Schema.define(version: 20190709193155) do
     t.integer "pages"
   end
 
+  create_table "collection_books", force: :cascade do |t|
+    t.bigint "book_id"
+    t.bigint "collection_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_collection_books_on_book_id"
+    t.index ["collection_id"], name: "index_collection_books_on_collection_id"
+  end
+
+  create_table "collections", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "collection_books", "books"
+  add_foreign_key "collection_books", "collections"
 end
