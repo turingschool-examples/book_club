7fd6628f8af7c5bd7ced87e0803bd0b533137e9d
Lain McGratH <lain.mcgrath@gusto.com>
2019-12-14 21:25:50 -0700

    Correct typo in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+author_1 = Author.create!(name: "Anthony Doerr")
+author_2 = Author.create!(name: "Stieg Larson")
+
+book_1 = author_1.books.create!(title: "All the Light We Cannot See",
+                                pages: 544,
+                                publication_year: 2014)
+
+book_2 = author_1.books.create!(title: "About Grace",
+                                pages: 432,
+                                publication_year: 2015)
+
+book_3 = author_2.books.create!(title: "The Girl with the Dragon Tattoo",
+                                pages: 463,
+                                publication_year: 2005)
+
+book_4 = author_2.books.create!(title: "The Girl Who Played With Fire",
+                                pages: 631,
+                                publication_year: 2006)
+
+book_5 = author_2.books.create!(title: "The Girl Who Kicked the Hornet's Nest",
+                                pages: 602,
+                                publication_year: 2007)
