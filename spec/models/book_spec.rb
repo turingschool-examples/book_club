acc58dab2909e2353e81f632bc07e8465330719c
Kevin Krato <kratok08@gmail.com>
2019-12-16 08:34:50 -0700

    Update book models tests

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe 'Book', type: :model do
+RSpec.describe Book, type: :model do
   # relations
-  it { should have_many(:authors) }
+  it { should have_many(:author_books) }
+  it { should have_many(:authors).through(:author_books) }
 
   # validations
   it { should validate_presence_of(:title) }
-  it { should validate_presence_of(:number_of_pages) }
+  it { should validate_presence_of(:pages) }
   it { should validate_presence_of(:publication_year) }
 end
