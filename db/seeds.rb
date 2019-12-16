61c4afe83043e9b783ded71ce236847521aaee9f
Kevin Krato <kratok08@gmail.com>
2019-12-16 08:33:55 -0700

    Update schema with authorBooks and create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+@author_1 = Author.create(
+  name: "Jack Ma"
+)
+@author_1.books.create(
+  title: "Starwars Book:1",
+  pages: 56,
+  publication_year: 1999
+)
+@author_1.books.create(
+  title: "Starwars Book:2",
+  pages: 107,
+  publication_year: 2001
+)
